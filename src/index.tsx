ce29d7c8edbc4f8418933a1f5a0b1a59e6a164e0
Chris Mellard <chris.mellard@icloud.com>
2021-05-22 21:22:20 +1200

    gitignore

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Auth0Provider} from '@auth0/auth0-react';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <Auth0Provider
+            domain="dev-0vfqiafx.au.auth0.com"
+            clientId="d7JGpSyXG8DYSBSfD0csAPtR3dOsceHg"
+            redirectUri={window.location.origin}
+        >
+            <App/>
+        </Auth0Provider>
+    </React.StrictMode>,
+    document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
